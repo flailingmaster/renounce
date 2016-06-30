9f35f552bdfa4b578d65c4bb6d067547357d81ab
Raymond Wang <raymond@blinkingtwelve.net>
2016-06-29 21:52:30 -0400

    added a reports migration schema

---
+++ database/migrations/2016_06_30_012430_create_reports_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('reports', function (Blueprint $table) {
+        $table->increments('id');
+        $table->timestamps();
+        $table->date('publication_date');
+        $table->string('type');
+        $table->string('public_inspection_pdf_url');
+        $table->string('html_url');
+        $table->string('pdf_url');
+        $table->string('full_text_xml_url');
+        $table->mediumText('full_text_xml');
+        $table->string('title');
+        $table->mediumText('excerpts');
+        $table->mediumText('agencies');
+        $table->string('document_number');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('reports');
+    }
+}
