e380ef3ddb9484b1dab04c141bc039f417fd8e3b
Raymond Wang <raymond@blinkingtwelve.net>
2016-07-05 23:45:43 -0500

    stubbing out controllers for name and report

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('report', 'ReportController', ['only' => [
+    'index', 'show'
+]]);
+
+Route::resource('name', 'NameController', ['only' => [
+    'index', 'show'
+]]);
