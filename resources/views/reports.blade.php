5945f3b20480ca27f9e25579876bb0ca351cf9d9
Raymond Wang <raymond@blinkingtwelve.net>
2016-07-10 23:22:59 -0500

    added links to actual reports

--- resources/views/reports.blade.php
+++ resources/views/reports.blade.php
@@ -14,10 +14,10 @@
     <tbody>
       @foreach ($reports as $report)
         <tr>
-          <td>{{ $report->id }}</td>
+          <td><a href=/report/{{ $report->id }}>{{ $report->id }}</a></td>
           <td>{{ $report->publication_date }}</td>
-          <td>{{ $report->document_number }}</td>
-          <td colspan=2>{{ $report->title }}</td>
+          <td><a href=/report/{{ $report->id }}>{{ $report->document_number }}</a></td>
+          <td colspan=2><a href=/report/{{ $report->id }}>{{ $report->title }}</a></td>
           <td>{{ $report->names()->count() }}</td>
         </tr>
       @endforeach
