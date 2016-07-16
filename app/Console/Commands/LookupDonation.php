5fc7e3627e116cce031c21942daa627397a3ec35
Raymond Wang <raymond@blinkingtwelve.net>
2016-07-15 23:46:39 -0700

    made lookup capable of returning results in an array

--- app/Console/Commands/LookupDonation.php
+++ app/Console/Commands/LookupDonation.php
@@ -58,36 +58,41 @@ class LookupDonation extends Command
       // submit that form
       $crawler = $client->submit($form);
       $donation = [];
-      $crawler->filter('tbody > tr')->each(function ($node, $i) {
+      $crawler->filter('tbody > tr')->each(function ($node, $i) use (&$donation) {
         $row = [];
-        $node->filter('td')->each(function($inner, $i) {
-        switch($i) {
-          case 0:
-            $first = explode("<br>",$inner->html());
-            $row['name'] = $first[0];
-            $row['location'] = $first[1];
-            break;
-          case 1:
-            $row['occupation'] = $inner->text();
-            break;
-          case 2:
-            $row['date'] = $inner->text();
-            break;
-          case 3:
-            $row['amount'] = $inner->text();
-            break;
-          case 4:
-            $row['recipient'] = $inner->text();
-            break;
+        $cells = $node->filter('td');
 
-          default:
-            $this->info( "additional info: ".$inner->text());
-            break;
-          }
-          $this->info("this is the cell: \n".print_r($row, TRUE));
-        });
+        $inc = 0;
+        foreach ($cells as $element) {
+          switch($inc) {
+            case 0:
+              $first = explode("<br>",$element->ownerDocument->savehtml($element));
+              $row['name'] = str_replace("<td>", "", $first[0]);
+              $row['location'] = str_replace("</td>", "", $first[1]);
+              break;
+            case 1:
+              $row['occupation'] = $element->nodeValue;
+              break;
+            case 2:
+              $row['date'] = $element->nodeValue;
+              break;
+            case 3:
+              $row['amount'] = $element->nodeValue;
+              break;
+            case 4:
+              $row['recipient'] = $element->nodeValue;
+              break;
 
+            default:
+              $this->info( "additional info: ".$element->nodeValue);
+              break;
+            }
+            $inc++;
+        }
+          // dev buttress code
+          //$this->info("this is the row: \n".print_r($row, TRUE));
+          $donation[] = $row;
       });
-
+          $this->info("this is the donation: \n".print_r($donation, TRUE));
     }
 }
