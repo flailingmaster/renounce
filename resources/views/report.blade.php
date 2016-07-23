d4d471e2f543095526bfc871186d4c4ea28fad88
Raymond Wang <raymond@blinkingtwelve.net>
2016-07-22 23:13:50 -0500

    modified the names listing within reports.

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -22,13 +22,23 @@
   <thead>
     <tr>
       <th>ID</th>
+      <th>Document Number</th>
       <th colspan=2>Name</th>
+      <th>Donation Count</th>
+      <th>Queried?</th>
     </tr>
   </thead>
   <tbody>
-@foreach ($report->names as $name)
-    <tr><td>{{ $name->id }}</td><td colspan=2>{{ $name->name }}</td></tr>
-@endforeach
+    @foreach ($report->names as $name)
+      <tr>
+        <td><a href=/name/{{ $name->id }}>{{ $name->id }}</a></td>
+        <td>{{ $name->document_number }}</td>
+        <td colspan=2><a href=/name/{{ $name->id }}>{{ $name->name }}</a></td>
+        <td>{{ $name->raw_count }}</td>
+        <td>{{ $name->queried }}</td>
+      </tr>
+    @endforeach
+
   </tbody>
 </table>
 @endsection
