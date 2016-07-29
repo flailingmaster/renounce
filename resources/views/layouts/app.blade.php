e7e261c8fcfaae81505228dd88b241569098300d
Raymond Wang <raymond@blinkingtwelve.net>
2016-07-28 23:46:43 -0700

    added a truly heinous navigation bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,22 @@
   </head>
 
   <body>
+    <header class="centered-navigation" role="banner">
+  <div class="centered-navigation-wrapper">
+    <a href="javascript:void(0)" class="mobile-logo">
+      <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_square.png" alt="Logo image">
+    </a>
+    <a href="javascript:void(0)" id="js-centered-navigation-mobile-menu" class="centered-navigation-mobile-menu">MENU</a>
+    <nav role="navigation">
+      <ul id="js-centered-navigation-menu" class="centered-navigation-menu show">
+        <li class="nav-link"><a href="/">Reports</a></li>
+        <li class="nav-link"><a href="/name">Names</a></li>
+        <li class="nav-link"><a href="/name/donated">Filtered Names</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
+
     @yield('content')
   </body>
 </html>
