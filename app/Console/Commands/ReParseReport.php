eac59cfed0b71be2994f0261493f1efe0bdb5bc3
Raymond Wang <raymond@blinkingtwelve.net>
2016-08-04 21:32:27 -0400

    ugly, but it works. extended reparsing reports to give the option to add names

--- app/Console/Commands/ReParseReport.php
+++ app/Console/Commands/ReParseReport.php
@@ -12,7 +12,7 @@ class ReParseReport extends Command
      *
      * @var string
      */
-    protected $signature = 'report:reparse {--details}';
+    protected $signature = 'report:reparse {--details} {--no-prompt}';
 
     /**
      * The console command description.
@@ -40,23 +40,46 @@ class ReParseReport extends Command
     {
         //
         $this->info('Finds reports with associated names');
-        $empty_reports = DB::select('select reports.id as id, reports.document_number as doc, reports.full_text_xml_url as xmlurl, reports.full_text_xml as full_text_xml from reports left join names on reports.document_number = names.document_number group by reports.id having count(names.id) = 0');
+        $empty_reports = DB::select('select reports.document_number as document_number, reports.id as id, reports.document_number as doc, reports.full_text_xml_url as xmlurl, reports.full_text_xml as full_text_xml from reports left join names on reports.document_number = names.document_number group by reports.id having count(names.id) = 0');
         foreach($empty_reports as $report) {
           $this->info('id:'.$report->id."\tdoc:".$report->doc."\turl:".$report->xmlurl);
           if ($report->full_text_xml != "" && $this->option('details')) {
             //$this->info($report->full_text_xml);
             $crawler = new Crawler($report->full_text_xml);
             $crawler = $crawler->filter('FP');
+            $output_array = [];
             foreach($crawler as $name) {
-              $this->info($name->nodeValue);
+              //$this->info($name->nodeValue);
+              $output_array[] = ['document_number' => $report->document_number, 'name' => $name->nodeValue, 'name_html' => $name->ownerDocument->savehtml($name)];
             }
-
+            $this->table(['document_number', 'name', 'name_html'], $output_array);
+            if (count($output_array) > 0)
+            {
+              $use_names = $this->ask('Create names from this table?');
+              if ($use_names == 'Y' || $use_names == 'y') {
+                $this->add_names($output_array);
+              }
+            }
           } else {
             $this->info('no xml found');
           }
 
-          $continue = $this->ask('continue?');
-          if ($continue == 'n') break;
+          if ($this->option('no-prompt') == false) {
+            $continue = $this->ask('Continue? [y|n]');
+            if ($continue == 'n') break;
+          }
         }
+    }
+
+    public function add_names($name_array)
+    {
+      foreach ($name_array as $name)
+      {
+        $this->info('doc: '.$name['document_number']."\t\tname: ".$name['name']);
+        Name::create(array(
+          'name' => $name['first_name'],
+          'document_number' => $name['document_number'],
+        ));
+      }
     }
 }
