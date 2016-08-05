2919e6a61b6b5932fb6630990b513156a4c7590a
Raymond Wang <raymond@blinkingtwelve.net>
2016-08-04 20:33:44 -0400

    added more options for reparse command

--- app/Console/Commands/ReParseReport.php
+++ app/Console/Commands/ReParseReport.php
@@ -12,7 +12,7 @@ class ReParseReport extends Command
      *
      * @var string
      */
-    protected $signature = 'report:reparse';
+    protected $signature = 'report:reparse {--details}';
 
     /**
      * The console command description.
@@ -39,23 +39,24 @@ class ReParseReport extends Command
     public function handle()
     {
         //
-        $this->info('Basic Test');
+        $this->info('Finds reports with associated names');
         $empty_reports = DB::select('select reports.id as id, reports.document_number as doc, reports.full_text_xml_url as xmlurl, reports.full_text_xml as full_text_xml from reports left join names on reports.document_number = names.document_number group by reports.id having count(names.id) = 0');
         foreach($empty_reports as $report) {
           $this->info('id:'.$report->id."\tdoc:".$report->doc."\turl:".$report->xmlurl);
-          if ($report->full_text_xml != "") {
+          if ($report->full_text_xml != "" && $this->option('details')) {
             //$this->info($report->full_text_xml);
             $crawler = new Crawler($report->full_text_xml);
             $crawler = $crawler->filter('FP');
             foreach($crawler as $name) {
               $this->info($name->nodeValue);
             }
-            $continue = $this->ask('continue?');
-            if ($continue == 'n') break;
+
           } else {
             $this->info('no xml found');
           }
 
+          $continue = $this->ask('continue?');
+          if ($continue == 'n') break;
         }
     }
 }
