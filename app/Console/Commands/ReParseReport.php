f0664383508a2f77005cb092abb4a4e7c753ec17
Raymond Wang <raymond@blinkingtwelve.net>
2016-08-13 12:24:45 -0700

    added note for later work

--- app/Console/Commands/ReParseReport.php
+++ app/Console/Commands/ReParseReport.php
@@ -40,10 +40,12 @@ class ReParseReport extends Command
      */
     public function handle()
     {
-        //
+
         $this->info('Finds reports with associated names');
         $empty_reports = DB::select('select reports.document_number as document_number, reports.id as id, reports.document_number as doc, reports.full_text_xml_url as xmlurl, reports.full_text_xml as full_text_xml from reports left join names on reports.document_number = names.document_number group by reports.id having count(names.id) = 0');
         $names_added = 0;
+
+        // TODO: this is horrendously ugly. Need to organize so reports can be reparsed
         foreach($empty_reports as $report) {
           $this->info('id:'.$report->id."\tdoc:".$report->doc."\turl:".$report->xmlurl);
           if ($report->full_text_xml != "" && $this->option('details')) {
